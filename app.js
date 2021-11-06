a9f9d70398638eb1f1b3e584ec1dfdf666ce0dbc
Nurgeldi Tuwakow <nt.interpretor>
2021-11-06 11:15:56 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+//HIGH ORDER FUNCTIONS
+//SORT
+let list = [10, 20, 1, 2, 3, 4, 56, 45, 23, 1, 2, 3, 4, 5, 6, 7, 0].sort(
+  (a, b) => {
+    return a - b;
+  }
+);
+console.log(list);
+
+//REDUCE
+
+//FOREACH
+
+//MAP
+
+//FILTER
+
+//FLAT
+
+//SOME
+//EVERY
+//INDEXOF
+//...
