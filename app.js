d5c36fbb179a4b8a9f7ff56108ddfb8153fa60ed
Nurgeldi Tuwakow <nt.interpretor>
2021-11-06 11:57:54 +0500

    Added some methods name

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//HIGH ORDER FUNCTIONS
+//------------- HIGH ORDER FUNCTIONS -------------
 //SORT
 let list = [10, 20, 1, 2, 3, 4, 56, 45, 23, 1, 2, 3, 4, 5, 6, 7, 0].sort(
   (a, b) => {
@@ -8,16 +8,35 @@ let list = [10, 20, 1, 2, 3, 4, 56, 45, 23, 1, 2, 3, 4, 5, 6, 7, 0].sort(
 console.log(list);
 
 //REDUCE
-
 //FOREACH
-
 //MAP
-
 //FILTER
-
 //FLAT
-
 //SOME
 //EVERY
 //INDEXOF
-//...
+//INDEX
+//FIND
+
+//------------- ARRAY METHODS -------------
+//POP
+//PUSH
+//SHIFT
+//UNSHIFT
+//SPLICE
+//SPLIT
+//SLICE
+//INDEXOF
+//ISARRAY
+//ARRAY.FROM
+//OF
+//OBSERVE
+//LENGTH
+//PROTOTYPE
+
+// ------------- STRING METHODS -------------
+//------------- OBJECT METHODS -------------
+//OBJECT KEY
+//OBJECT VALUE
+//OBJECT.OBSERVE
+//------------- NUMBER METHODS -------------
