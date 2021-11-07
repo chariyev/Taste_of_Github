6fe13b36a3fe0001a49fac887acb6650dc106c70
Nurgeldi Tuwakow <nt.interpretor@gmail.com>
2021-11-07 13:10:40 +0500

    Some sql terms was explained

--- L_sql.sql
+++ L_sql.sql
@@ -1,3 +1,10 @@
 --FOCUSING ON SQL, ESPECIALLY ON MSSQL --
 -- LEARNING SQL TERMS AND APPLYING IT IN PRACTICE --
 
+USE Northwind -- database in use
+
+SELECT * FROM Musteriler -- select all columns from specified tables
+
+
+
+SELECT adi, soyadi FROM Personeller -- adi, soyadi columns was displayed
