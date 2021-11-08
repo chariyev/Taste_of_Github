3a4206fa3ce3d9d196fe64fe3c02eeabe3530a4c
Nurgeldi Tuwakow <nt.interpretor>
2021-11-08 16:18:49 +0500

    Added config folder

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const customers = require("./routes/customers");
 //MIDDLEWARE SECTION
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use("/", home);
 app.use("/api/v1/products", products);
 app.use("/api/v1/customers", customers);
